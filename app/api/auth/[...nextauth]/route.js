0cc921adb2586112cf227920e237565a16bb76ca
Ashish Agrawal <agrawalashish.in@gmail.com>
2024-12-08 14:34:51 +0530

    feat: update next-auth route api for connecting to mongodb database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import nextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database';
 
 const handler = nextAuth({
   providers: [
@@ -12,7 +13,24 @@ const handler = nextAuth({
 
   },
   async signIn({ profile }) {
+    try {
+      /**
+        Every Next JS route is knwon as a ServerLess Route.
+        Which means that this is a lambda function which opens up only when it is called
+        So every time it gets called, its need to spinup the server and make a connection to the database.
+        This is great so that we don't have to keep our server running.
+      */
+      await connectToDB();
 
+      // check if a user already exists
+
+      // if not, create a new user
+
+      return true;
+    } catch (error) {
+      console.log('An unexpected error occured', error);
+      return false;
+    }
   }
 });
 
