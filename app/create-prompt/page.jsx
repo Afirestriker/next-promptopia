7bfb03cd2faa0624dd37ec98a8bf1d3fd353aadc
Ashish Agrawal <agrawalashish.in@gmail.com>
2024-12-14 11:18:43 +0530

    feat(create-prompt): design create prompt form page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [prompt, setPrompt] = useState({
+    prompt: '',
+    tag: ''
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: session?.user.id,
+          prompt: prompt.prompt,
+          tag: prompt.tag,
+        })
+      });
+
+      if (response.ok) router.push('/');
+    } catch (error) {
+      console.error("Error while submitting form", error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
   return (
-    <div>CreatePrompt</div>
+    <Form
+      type="Create"
+      post={prompt}
+      setPost={setPrompt}
+      isSubmitting={isSubmitting}
+      handleSubmit={createPrompt}
+    />
   )
 }
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,71 @@
-const Form = () => {
+import Link from 'next/link';
+
+const Form = ({
+  type,
+  post,
+  setPost,
+  isSubmitting,
+  handleSubmit
+}) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the world,
+        and let your imagination run wild with any
+        AI-Powered platform
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+        </label>
+
+        <textarea
+          className="form_textarea"
+          value={post.prompt}
+          onChange={e => setPost({...post, prompt: e.target.value})}
+          placeholder="write your prompt here"
+          required="required"
+        />
+
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag {' '}
+            <span className="font-normal">(#product, #webDevelopment, #idea)</span>
+          </span>
+        </label>
+
+        <input
+          className="form_input"
+          value={post.tag}
+          onChange={e => setPost({...post, tag: e.target.value})}
+          placeholder="#tag"
+          required="required"
+        />
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500">
+            Cancel
+          </Link>
+
+          <button
+            type="button"
+            className="px-5 py-1.5 text-sm rounded-full bg-primary-orange text-white"
+          >
+            {isSubmitting ? `${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
 
 export default Form;
