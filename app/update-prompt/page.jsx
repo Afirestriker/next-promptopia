87166304b605bc7caa0123448638e07a359f9d5e
Ashish Agrawal <agrawalashish.in@gmail.com>
2024-12-15 16:15:14 +0530

    🚀(update-prompt): complete update-prompt functionality

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,8 +1,58 @@
+"use client";
 
-const updatePrompt = () => {
+import { useState, useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import Form from "@components/Form";
+
+const updatePrompt = ({ params }) => {
+  const [prompt, setPrompt] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const promptId = searchParams.get('id');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(`/api/prompt/${promptId}/`, { method: 'GET' });
+        const promptData = await response.json();
+        setPrompt(promptData);
+      } catch (error) {
+        console.error("Failed to load prompt data", error);
+      }
+    })();
+  }, []);
+
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch(`/api/prompt/${promptId}/`, {
+        method: 'PATCH',
+        body: JSON.stringify(prompt)
+      });
+
+      if (response.ok) router.push('/profile');
+    } catch (error) {
+      console.error('Failed to update the prompt', error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <div>Update Prompt</div>
-  )
-}
+    <Form
+      type="Update"
+      post={prompt}
+      setPost={setPrompt}
+      isSubmitting={isSubmitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
 
-export default updatePrompt
+export default updatePrompt;
