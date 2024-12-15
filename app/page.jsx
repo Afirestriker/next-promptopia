d64ad181696d44e87229f081b0830dbd9afbb4e0
Ashish Agrawal <agrawalashish.in@gmail.com>
2024-12-15 12:25:41 +0530

    chore(landing-page): remove line

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,6 @@ const Home = () => {
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for moden world to discover, create and share creative prompts
       </p>
-
       <Feed />
     </section>
   );
